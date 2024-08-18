32aa1fae8a8493d78d14f23ed5f2af099b2e9d20
Ganesh <ganeshkutty2257767@gmail.com>
2024-08-18 21:01:01 +0530

    Changed flask-mysqldb to use mysql-connector-python. Added table to the bottom

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
 from flask import Flask, render_template, request, url_for, flash
-from flask_mysqldb import MySQL
-from MySQLdb import IntegrityError
+import mysql.connector
+from mysql.connector.errors import IntegrityError
 
-app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/")
+mydb = mysql.connector.connect(
+    host="mysql.railway.internal",
+    user="root",
+    password="AlciOgdqfDZHzmBWPtuGDrYpuglPEqme",
+    database="railway"
+)
 
-app.config['SECRET_KEY'] = 'hhLlPl"0aku}Yh0dk^FDJ-=Vq&V>;S=0h1~5j2Zct4M>X$:ZoWSJ-__(B3jLuV('
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="root",
+#     database="freshii"
+# )
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'root'
-# app.config['MYSQL_DB'] = 'freshii'
+app = Flask(__name__, template_folder="templates",
+            static_folder="static", static_url_path="/")
 
-app.config['MYSQL_HOST'] = 'mysql.railway.internal'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'AlciOgdqfDZHzmBWPtuGDrYpuglPEqme'
-app.config['MYSQL_DB'] = 'railway'
-# mysql.init_app(app)
-mysql = MySQL(app)
+app.config[
+    'SECRET_KEY'] = 'hhLlPl"0aku}Yh0dk^FDJ-=Vq&V>;S=0h1~5j2Zct4M>X$:ZoWSJ-__(B3jLuV('
 
+
+cursor = mydb.cursor(buffered=True)
+def refresh_data():
+    cursor.execute("SELECT * from freshii_daily")
+    data = cursor.fetchall()
+    return data
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-
-    with mysql.connection.cursor() as cursor:
-        cursor.execute("SELECT * from freshii_daily")
-        data = cursor.fetchone()
-        
-    if request.method == "GET":
-        return render_template("index.html")
-    
     if request.method == "POST":
-        data = request.form
-        
+        form_data = request.form
+
         sql = "INSERT INTO freshii_daily (date, c1, c5, c10, c25, c50, d1, d2, d5, d10, d20, d50, d100, bag_amount, locker_amount) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        val = (data["date"], data["c1"], data["c5"], data["c10"], data["c25"], data["c50"], data["d1"], data["d2"], data["d5"], data["d10"], data["d20"], data["d50"], data["d100"], data["bag"], data["locker"])
-        
-        with mysql.connection.cursor() as cursor:
-            try:
-                cursor.execute(sql, val)
-            except IntegrityError as e:
-                flash(f'Data for this date: {data["date"]} already exists', 'error')
-                
-        mysql.connection.commit()
-    
-    print(data)
-    return render_template("index.html")
+        val = (form_data["date"], form_data["c1"], form_data["c5"], form_data["c10"], form_data["c25"], form_data["c50"], form_data["d1"], form_data["d2"],
+               form_data["d5"], form_data["d10"], form_data["d20"], form_data["d50"], form_data["d100"], form_data["bag"], form_data["locker"])
+
+        try:
+            cursor.execute(sql, val)
+        except IntegrityError as e:
+            flash(f'Data for this date: {form_data["date"]} already exists', 'error')
+
+        mydb.commit()
+
+    data = refresh_data()
+    return render_template("index.html", data=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
