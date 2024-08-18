be62e66b90d6646ebca8d595c23c4977143c3b45
Ganesh <ganeshkutty2257767@gmail.com>
2024-08-18 21:46:19 +0530

    1. Added success message for successful entries. 2. Unique key violation now updates the entry as opposed to throwing an error

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ from flask import Flask, render_template, request, url_for, flash
 import mysql.connector
 from mysql.connector.errors import IntegrityError
 
-mydb = mysql.connector.connect(
-    host="mysql.railway.internal",
-    user="root",
-    password="AlciOgdqfDZHzmBWPtuGDrYpuglPEqme",
-    database="railway"
-)
+# TODO
+# 1. Add success message on successfully adding a record
+# 2. Optional: Scroll to the table after adding a record
+# 3. Update the record if the date already exists
 
 # mydb = mysql.connector.connect(
-#     host="localhost",
+#     host="mysql.railway.internal",
 #     user="root",
-#     password="root",
-#     database="freshii"
+#     password="AlciOgdqfDZHzmBWPtuGDrYpuglPEqme",
+#     database="railway"
 # )
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="freshii"
+)
 
 app = Flask(__name__, template_folder="templates",
             static_folder="static", static_url_path="/")
@@ -41,7 +46,15 @@ def index():
         try:
             cursor.execute(sql, val)
         except IntegrityError as e:
-            flash(f'Data for this date: {form_data["date"]} already exists', 'error')
+            print("Except")
+            sql = "UPDATE freshii_daily SET c1 = %s, c5 = %s, c10 = %s, c25 = %s, c50 = %s, d1 = %s, d2 = %s, d5 = %s, d10 = %s, d20 = %s, d50 = %s, d100 = %s, bag_amount = %s, locker_amount = %s WHERE date = %s"
+            val = (form_data["c1"], form_data["c5"], form_data["c10"], form_data["c25"], form_data["c50"], form_data["d1"], form_data["d2"], form_data["d5"], form_data["d10"], form_data["d20"], form_data["d50"], form_data["d100"], form_data["bag"], form_data["locker"], form_data["date"])
+            cursor.execute(sql, val)
+            flash(f'Data for this date: {form_data["date"]} already exists, data has been updated', 'info')
+
+        else:
+            print("Else")
+            flash('Data added successfully', 'success')
 
         mydb.commit()
 
