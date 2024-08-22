c727d7352092d21d4997fd1e805bd41e213d0a07
Ganesh <ganeshkutty2257767@gmail.com>
2024-08-22 23:17:45 +0530

    Removed debug code

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,6 @@ var totalElement = document.getElementById("total");
 var allNumeric = document.querySelectorAll('input[type="number"]');;
 
 function calculate_all_value() {
-    // allNumeric = 
     var total = 0;
     for (var element of allNumeric) {
 
@@ -33,33 +32,6 @@ function calculate_all_value() {
             dropElement.value = 0.00;
         }
     }
-    // var total = 0;
-    // for (var element of allNumeric) {
-        
-    //     var id_ = element.attributes.id.value;
-    //     var denomination_value = id_.slice(1, id_.length);
-        
-        
-    //     final_value = isNaN(parseFloat(element.value).toFixed(2)) ? 0 : parseFloat(denomination_value).toFixed(2) * parseFloat(element.value).toFixed(2);
-    //     total = total + final_value;
-    //     total =  parseFloat(total).toFixed(2)
-    //     console.log({
-    //         id_: id_,
-    //         denomination_value: denomination_value,
-    //         final_value: final_value,
-    //         total: total
-    //     });
-
-    //     totalElement.value = total;
-    //     var dropAmount = total - 150;
-    //     if (dropAmount < 0) {
-    //         dropElement.value = 0;
-    //     }
-    //     else {
-    //         dropElement.value = parseFloat(dropAmount).toFixed(2);
-    //     }
-    // }
-
 }
 
 for (var element of allNumeric) {
