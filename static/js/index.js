6bbad6446c4687ed375ab56597e2a78d975359c1
Ganesh <ganeshkutty2257767@gmail.com>
2024-08-23 16:20:46 +0530

    Made the labels a little dynamic, refactored the javascript code to make it cleaner

--- static/js/index.js
+++ static/js/index.js
@@ -1,40 +1,45 @@
-var dropElement = document.getElementById("drop");
-var totalElement = document.getElementById("total");
-var allNumeric = document.querySelectorAll('input[type="number"]');;
-
-function calculate_all_value() {
-    var total = 0;
-    for (var element of allNumeric) {
-
-        var id_ = element.attributes.id.value;
-    
-        var dollarValueTemp = parseFloat(parseFloat(id_.slice(1, id_.length)).toFixed(2));
-        console.log("Type of dollarValueTemp: ", dollarValueTemp, typeof(dollarValueTemp));
-    
-        var dollarValue = isNaN(dollarValueTemp) ? 0 : dollarValueTemp;
-        console.log("Type of dollarValue: ", dollarValue, typeof(dollarValue));
-    
-        var inputValue = isNaN(element.valueAsNumber) ? 0 : element.valueAsNumber;
-    
-        total = parseFloat(parseFloat(total + (dollarValue * inputValue)).toFixed(2));
-        console.log({
-                    id_: id_,
-                    dollarValue: dollarValue,
-                    inputValue: inputValue,
-                    total: total
-                });
-    
-        totalElement.value = total;
-        if (total > 150) {
-            dropElement.value = parseFloat((total - 150).toFixed(2));
-        }
-        else{
-            dropElement.value = 0.00;
-        }
+function calculateDollarValue(inputId) {
+    const dollarValueTemp = parseFloat(inputId.slice(1, inputId.length));
+    return Number.isNaN(dollarValueTemp) ? 0 : dollarValueTemp;
+  }
+  
+  function calculateGrandTotal(allNumeric) {
+    let grandTotal = 0;
+    for (const element of allNumeric) {
+      const inputId = element.attributes.id.value;
+      const dollarValue = calculateDollarValue(inputId);
+      const inputValue = Number.isNaN(element.valueAsNumber) ? 0 : element.valueAsNumber;
+      updateLabel(inputValue, element, inputId);
+      grandTotal = parseFloat((grandTotal + (dollarValue * inputValue)).toFixed(2));
     }
-}
+    return grandTotal;
+  }
+  
+  function updateElements(grandTotal) {
+    const totalElement = document.getElementById("total");
+    const dropElement = document.getElementById("drop");
+    totalElement.value = grandTotal;
+    if (grandTotal > 150) {
+      dropElement.value = parseFloat((grandTotal - 150).toFixed(2));
+    } else {
+      dropElement.value = 0;
+    }
+  }
 
-for (var element of allNumeric) {
-    // element.addEventListener('change', calculate_drop_value);
-    element.addEventListener('input', calculate_all_value);
-}
+  function updateLabel(inputValue, inputElement, inputId) {
+    var inputLabel = inputElement.labels[0];
+    console.log(inputElement);
+    if (inputValue) {
+        labelText = `${inputLabel.htmlFor.replace('d', '$')} x ${inputValue} = $${calculateDollarValue(inputId) * inputValue}`;
+        inputLabel.innerHTML = labelText;
+    }
+    else {
+        inputLabel.innerHTML = inputLabel.htmlFor.replace('d', '$');
+    }
+  }
+  
+  function calculateAllValue() {
+    const allNumeric = document.querySelectorAll('input[type="number"]');
+    const grandTotal = calculateGrandTotal(allNumeric);
+    updateElements(grandTotal);
+  }
